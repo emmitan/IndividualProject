d9c5d0cd199cb19f9eb228463b4b9cc82613cf18
emmitan <142547879+emmitan@users.noreply.github.com>
2024-03-21 10:23:53 -0600

    Update fairy.py

--- src/fairy.py
+++ src/fairy.py
@@ -1,1 +1,21 @@
+import pygame
 
+pygame.init()
+
+class Fairy:
+  def __init__(self, x, y, pos, speed):
+    self.x, self.y, self.pos, self.speed = x, y, pos, speed
+
+  def display(x, y):
+    #load imgs
+    #f1 = pygame.image.load("fairy.png")
+    #f1 = pygame.transform.scale(f1, (50,50))
+    #f2 = pygame.image.load("fairy.png")
+    #f2 = pygame.transform.scale(f2, (50,50))
+    #f3 = pygame.image.load("fairy.png")
+    #f3 = pygame.transform.scale(f3, (50,50))
+
+    #display imgs
+
+    #if imgs are with the plant
+    pass
