aea3db0737fe79058e2d3b568e9c9ae1ea3f6349
emmitan <142547879+emmitan@users.noreply.github.com>
2024-04-18 10:31:23 -0600

    Update plant.py

--- src/plant.py
+++ src/plant.py
@@ -1,8 +1,23 @@
 import pygame
 
 pygame.init()
+surface = pygame.display.get_surface()
+col = (255, 255, 255)
+center = (surface.get_width() / 2, surface.get_height() / 2)
+radius = 5
 
 class Plant:
-  def __init__(self, x, y, species, grow):
-    self.x, self.y, self.species = x, y, species
-    self.grow = grow
+  def __init__(self, x, y):
+    self.x, self.y = x, y
+    #self.grow = grow
+    x = 0
+    y = 0
+    
+
+  def display(self):
+    pygame.draw.circle(surface, col, center, radius)
+
+  def clicks(self):
+    clicks = 0
+    clicks += 1
+    print("clicks: ", clicks)
