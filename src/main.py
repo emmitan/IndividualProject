d94feb2a581a708e45cc16078b5116209e65e5db
emmitan <142547879+emmitan@users.noreply.github.com>
2024-04-18 10:31:40 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
 import pygame
-#from plant import Plant
+from plant import Plant
 #from fairy import Fairy
 
 pygame.init()
 
 start = False
-screen = pygame.display.set_mode([700, 500])
+screen = pygame.display.set_mode([500, 700])
 pygame.display.set_caption("Bloom")
 bg = pygame.image.load("sbgFiller.jpg")
+bg = pygame.transform.scale(bg, (500, 700))
 screen.blit(bg, (0, 0))
+p = Plant(0, 0)
 pygame.display.flip()
 
 running = True
+
 while running:
   if pygame.key.get_pressed()[pygame.K_SPACE]:
     start = True
 
   if start == True:
     bg = pygame.image.load("lbgFiller.jpg")
+    bg = pygame.transform.scale(bg, (500, 700))
     screen.blit(bg, (0, 0))
+    p.display()
     pygame.display.flip()
 
   else:
+    start = False
     bg = pygame.image.load("sbgFiller.jpg")
+    bg = pygame.transform.scale(bg, (500, 700))
     screen.blit(bg, (0, 0))
     pygame.display.flip()
 
