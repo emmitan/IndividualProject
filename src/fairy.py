158867de2c5092cda68f55b13cc3d03246cd1605
emmitan <142547879+emmitan@users.noreply.github.com>
2024-04-30 09:53:48 -0600

    Update fairy.py

--- src/fairy.py
+++ src/fairy.py
@@ -1,12 +1,17 @@
 import pygame
 
 pygame.init()
+screen = pygame.display.set_mode([700, 500])
+
 
 class Fairy:
-  def __init__(self, x, y, pos, speed):
-    self.x, self.y, self.pos, self.speed = x, y, pos, speed
 
-  def display(x, y):
+  def __init__(self, x, y, speed):
+    self.x, self.y, self.speed = x, y, speed
+
+  def display(self, x, y):
+    self.x = x
+    self.y = y
     #load imgs
     #jerry = pygame.image.load("jerry.png")
     #jerry = pygame.transform.scale(jerry, (50,50))
@@ -14,16 +19,18 @@ class Fairy:
     #thimble = pygame.transform.scale(thimble, (50,50))
     #petunia = pygame.image.load("petunia.png")
     #petunia = pygame.transform.scale(petunia, (50,50))
+    gnome = pygame.image.load("gnome.png")
+    gnome = pygame.transform.scale(gnome, (50, 50))
 
     #display imgs
     #screen.blit(jerry, (x, y))
     #screen.blit(thimble, (x, y))
     #screen.blit(petunia, (x, y))
-    #pygame.display.flip()
+    screen.blit(gnome, (x, y))
+    pygame.display.flip()
 
     #jerry: brownie, geraniums
     #thimble: pixie, pansy
     #petunia: gnome, petunia
-    
+
     #if imgs are with the plant
-    pass
