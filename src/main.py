c4013b6a7fcaffe7389fca81e62633bd2d504117
emmitan <142547879+emmitan@users.noreply.github.com>
2024-05-09 10:17:04 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,13 @@
-print("Welcome! Please enter a character's name: ")
-character = input()
+from mystery import Mystery
+#mystery = Mystery("Jane", 3)
 
-print("What kind of setting would you like? A for murder mystery, B for science fiction, C for fantasy. ")
+print(
+    "What kind of setting would you like? A for murder mystery, B for science fiction, C for fantasy.")
 letter = input()
 if letter == "A":
   print("You've chosen murder mystery.")
+  #mystery.prompt()
 if letter == "B":
   print("You've chosen science fiction.")
 if letter == "C":
   print("You've chosen fantasy.")
-  
-print("How many prompts would you like? (please pick from 1-10): ")
-num = int(input())
-if num == 1:
-  print("You've chosen 1 prompt.")
-    
