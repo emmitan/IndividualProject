d6d25964e740b09446886f5b5cf083d694ad3e60
emmitan <142547879+emmitan@users.noreply.github.com>
2024-05-10 13:55:34 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from mystery import Mystery
-#mystery = Mystery("Jane", 3)
+
+mystery = Mystery()
 
 print(
-    "What kind of setting would you like? A for murder mystery, B for science fiction, C for fantasy.")
+    "What kind of setting would you like? A for murder mystery, B for science fiction, C for fantasy: ")
 letter = input()
 if letter == "A":
   print("You've chosen murder mystery.")
-  #mystery.prompt()
+  mystery.prompts()
 if letter == "B":
   print("You've chosen science fiction.")
 if letter == "C":
